77def05ba68291a1940ff19a26322346d4b6c6a5
moveold0611 <b44sup97045@gmail.com>
2024-01-07 23:42:47 +0900

    first commit

---
+++ src/main/java/com/toyboard/board_toy_project/BoardToyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.toyboard.board_toy_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardToyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardToyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toyboard/board_toy_project/BoardToyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyboard.board_toy_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardToyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
